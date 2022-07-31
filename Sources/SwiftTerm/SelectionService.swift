playstonex/SwiftTerm

commit 7a88cfee0b7183bb9f5d4e6ea6f53f06055ac99e

Only raise the selection changed event if the active state did change

Sources/SwiftTerm/SelectionService.swift
@@ -36,8 +36,8 @@ class SelectionService: CustomDebugStringConvertible {
         set(newValue) {
             if _active != newValue {
                 _active = newValue
+                terminal.tdel?.selectionChanged (source: terminal)
             }
-            terminal.tdel?.selectionChanged (source: terminal)
             if active == false {
                 pivot = nil
             }

commit f6b7e8d8d48640dcda88a2a8861b1ba2644d4210

Fix the regression where the selection was no longer being drawn, introduced this regression in 7a88cfee0b7183bb9f5d4e6ea6f53f06055ac99e, fixes bug #246

Sources/SwiftTerm/SelectionService.swift
@@ -36,6 +36,10 @@ class SelectionService: CustomDebugStringConvertible {
         set(newValue) {
             if _active != newValue {
                 _active = newValue
+            }
+            // When we set active to true from inside the temrinal, we rely on selectionChanged being raised
+            // because the boundaries changed.
+            if active {
                 terminal.tdel?.selectionChanged (source: terminal)
             }
             if active == false {